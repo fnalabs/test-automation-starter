7dd879dcb1140f0fe2cbe77a99347ed94bd58af2
Adam Eilers <adam.eilers@gmail.com>
2019-03-14 08:21:47 -0500

    2.0.7 - update dependencies and scripts (#9)
    * updating dependencies and scripts
    
    * updating automation test example

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,9 @@
 // Selectors
 export const IMG_SELECTOR = '#hplogo'
 export const FORM_SELECTOR = '#tsf'
-export const INPUT_SELECTOR = '#lst-ib'
+export const INPUT_SELECTOR = 'input[name="q"]'
 export const RESULT_SELECTOR = '#res'
-export const LINK_SELECTOR = '#logocont a'
-export const TERMS_SELECTOR = 'a[href^="//www.google.com/intl/en_us/policies/terms/"]'
+export const LINK_SELECTOR = '#logo'
 
 // Fragments
 export const GOOGLE_FRAGMENT = Symbol('google fragment')
